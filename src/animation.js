d4fdb85377a7adbc8a8050eb229da1096975e17e
Unknown <iltstudios69@gmail.com>
2022-11-12 13:09:59 +0000

    Add files via upload

--- src/animation.js
+++ src/animation.js
@@ -1,3 +1,13 @@
+//Navbar //
+document.addEventListener('scroll', () => {
+    if(window.scrollY==0){
+        document.querySelector('.navbar').classList.remove('open');
+    }else{
+        document.querySelector('.navbar').classList.add('open');
+    }
+});
+//
+
 function isInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
@@ -5,7 +15,6 @@ function isInViewport(el) {
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
     );
 }
 
@@ -20,7 +29,7 @@ box.forEach(element => {
     
         if (messageText == true){
             new_box.classList.add('open');
-            document.querySelector('.about-heading').classList.add('.open');
+            document.querySelector('.about .heading').classList.add('.open');
         }
     }, {
         passive: true
@@ -52,26 +61,99 @@ burger.addEventListener('click', () => {
 const dropdown_menus = document.querySelector('.dropdowns');
 var identition = 0;
 var i = 0;
-var is_menu_open = false;
 const dropdown_subtext = [
-    "Created several websites with traffic over the hundreds. Won a few web design comps and have developed 2 websites, fullstack.",
-    "First technology i've learnt, have about 2 years of experience and have published about 4 - 5 indie games both mobile and pc.",
-    "Lately started producing software with technologies such as API's, Electron_JS, React Native, Node.JS and many more. Published a few.",
-    "Learnt web dev along with HTML, CSS. Style of writing both have changed from relaying on tags to relaying on felx box css and using countless divs.",
-    "Lately have started to love this language for its versatility, and easy writing. Have developed quite a few ",
-    "First language i've learnt, use it for hardware projects for its fast compile time and easy use.",
-    "Have developed quite a few games with C#, many of which through the unity development engine.",
-    "Data projects, GUI, API Contacts, Discord Bots, a majority of my time has been spent on using learning python.",
-    "Friend told me about rust, and ive loved it ever since. Haven't developed much other than simple basic programs.",
+    {"Created several websites with traffic over the hundreds. Won a few web design comps and have developed 2 websites, fullstack.":[
+        "This is a Test",
+        "This is a bigger test :D",
+    ]},
+    {"First technology i've learnt, have about 2 years of experience and have published about 4 - 5 indie games both mobile and pc.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"Lately started producing software with technologies such as API's, Electron_JS, React Native, Node.JS and many more. Published a few.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"Learnt web dev along with HTML, CSS. Style of writing both have changed from relaying on tags to relaying on felx box css and using countless divs.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"Lately have started to love this language for its versatility, and easy writing. Have developed quite a few Desktop Apps, Websites, and Mobile Apps.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"First language i've learnt, use it for hardware projects for its fast compile time and easy use.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"Have developed quite a few games with C#, many of which through the unity development engine.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"Data projects, GUI, API Contacts, Discord Bots, a majority of my time has been spent on using learning python.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
+    {"Friend told me about rust, and ive loved it ever since. Haven't developed much other than simple basic programs.":[
+        "This is a Test",
+        "This is a bigger test :D",        
+    ]},
 ]
+
 dropdown_menus.childNodes.forEach(element => {
     identition += 1;
     if(identition % 2 == 0){
+        //add dropdown info
         var ident_label = element.childNodes;
-        var subtext = document.createElement('p');
-        subtext.innerText = `${dropdown_subtext[i]}`;
+        var subtext = document.createElement('div');
+        subtext.classList.add('dropdown-subtext')
+        subtext.innerText = `${Object.keys(dropdown_subtext[i])}`;
         ident_label[1].appendChild(subtext);
         i += 1;
+
+        //add info card
+        // var infocard_parent = document.querySelector('.experience');
+        // infocard_parent.innerHTML = "";
+        // const subtext_infocard_value = Object.values(dropdown_subtext[i]);
+        // subtext_infocard_value[0].forEach(element => {
+        //     infocard_parent.appendChild(document.createTextNode(element));
+        // });
+
+        // animation
+        element.addEventListener('click', (e) => {
+            if(element.classList.contains("menu-dropdown")){
+                element.classList.remove('menu-dropdown');
+                element.classList.add('menu-dropdownopen');
+                element.childNodes.forEach((element, indent) => {
+                    if(element.childNodes[indent] != null){
+                        setTimeout(() => {
+                            element.childNodes[1].style.display = 'flex';
+                        }, 20);
+                    }
+                });
+            }else{
+                element.classList.remove('menu-dropdownopen');
+                element.classList.add('menu-dropdown');
+                element.childNodes.forEach((element, indent) => {
+                    if(element.childNodes[indent] != null){
+                        setTimeout(() => {
+                            element.childNodes[1].style.display = 'none';
+                        }, 200);
+                    }
+                });
+            }
+        });
     }
-})
-/** */
+});
+
+const skill_program = document.querySelector('.programs');
+console.log(skill_program);
+/* */
+
+//project animation
+document.querySelectorAll('.project-grid > div > img').forEach(element => {
+    element.addEventListener('click', (e) => {
+        e = e.target.src.split('/')[4].replace('.png', '');
+        console.log(e);
+    });
+});
--- src/spotify.js
+++ src/spotify.js
@@ -1,4 +1,111 @@
-// fetch("https://api.spotify.com/v1/users/t0ddfiv53v13oc0agbrashxlr")
-// .then(response => {
-//     console.log(response);
-// });
+// const APIController = (function() {
+//     const clientID = '1e2cd42d14c84adf9e6c38a10b46c0ab';
+//     const clientSecret = 'c365e3e8012a46a48061f55150dc2e09';
+
+//     const getToken = async () => {
+//         const result = await fetch('https://accounts.spotify.com/api/token', {
+//             method: 'POST',
+//             headers: {
+//                 "Content-Type": "application/x-www-form-urlencoded",
+//                 "Authorization" : "Basic" + btoa(clientID + ":" + clientSecret)
+//             },
+//             body: "grant_type-client-credentials"
+//         });
+
+//         const data = await result.json();
+//         console.log(data.access_token);
+//     }
+
+//     getToken();
+// })();
+
+// APIController();    
+const APIController = (function() {
+    
+    const clientId = '1e2cd42d14c84adf9e6c38a10b46c0ab';
+    const clientSecret = 'c365e3e8012a46a48061f55150dc2e09';
+
+    // private methods
+    const _getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data;
+    }
+    
+    const _getGenres = async (token) => {
+
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.categories.items;
+    }
+
+    const _getPlaylistByGenre = async (token, genreId) => {
+
+        const limit = 10;
+        
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.playlists.items;
+    }
+
+    const _getTracks = async (token, tracksEndPoint) => {
+
+        const limit = 10;
+
+        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.items;
+    }
+
+    const _getTrack = async (token, trackEndPoint) => {
+
+        const result = await fetch(`${trackEndPoint}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data;
+    }
+
+    return {
+        getToken() {
+            return _getToken();
+        },
+        getGenres(token) {
+            return _getGenres(token);
+        },
+        getPlaylistByGenre(token, genreId) {
+            return _getPlaylistByGenre(token, genreId);
+        },
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, trackEndPoint) {
+            return _getTrack(token, trackEndPoint);
+        }
+    }
+})();
+
+console.log(APIController)
